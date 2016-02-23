dcf2ce3589e2d4499286fff508bdebb49ae9d1d6
cassac <hicassac@gmail.com>
2016-02-23 11:49:19 -0800

    adds additional parameters to serializer fields

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,15 +6,16 @@ from accounts.models import MailingAddress
 class MailingAddressSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = MailingAddress
-		fields = ('first_name', 'last_name', 'address_line_1',
-			'address_line_2', 'city', 'state_province', 'phone',
-			'country')
+		fields = ('user', 'first_name', 'last_name', 'address_line_1',
+			'address_line_2', 'city', 'state_province', 'postal_code',
+			'country', 'phone')
 
 # Serializers define the API representation.
 class UserSerializer(serializers.ModelSerializer):
 
-	# mailingaddress_set = MailingAddressSerializer(many=True, read_only=True)
+	mailingaddress_set = MailingAddressSerializer(many=True, read_only=True)
 
 	class Meta:
 		model = User
-		fields = ('url', 'username', 'email', 'is_staff', 'mailingaddress_set')
+		fields = ('url', 'id', 'username', 'email', 'is_staff', 
+			'mailingaddress_set')
