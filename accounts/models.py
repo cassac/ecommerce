76c616128da0db8b30b876ce22444f08251c2235
cassac <hicassac@gmail.com>
2016-02-22 23:18:29 -0800

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.conf import settings
+
+class MailingAddress(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL)
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	address_line_1 = models.CharField(max_length=100)
+	address_line_2 = models.CharField(max_length=100)
+	city = models.CharField(max_length=50)
+	state_province = models.CharField(max_length=50)
+	postal_code = models.CharField(max_length=20)
+	country = models.CharField(max_length=50)
+	phone = models.CharField(max_length=20)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return "<MailingAddress: %d - %s>" % (self.user.id, self.user.username)	
---
+++ ecommerce/urls.py
@@ -0,0 +1,59 @@
+"""ecommerce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+from accounts.models import MailingAddress
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
+
+class MailingAddressSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = MailingAddress
+		fields = ('first_name', 'last_name', 'address_line_1',
+			'address_line_2', 'city', 'state_province', 'phone',
+			'country')
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+	mailingaddress_set = MailingAddressSerializer(many=True, read_only=True)
+
+	class Meta:
+		model = User
+		fields = ('url', 'username', 'email', 'is_staff', 'mailingaddress_set')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
