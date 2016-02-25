ae0dcd6861de85fc079538001a414c0ddc4f4f90
cassac <hicassac@gmail.com>
2016-02-25 09:19:13 -0800

    combines urls into one file to utilize DefaultRouter to display all available root endpoints on root api resource.

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework.routers import DefaultRouter
+
+from accounts import views as AccountsViews
+from products import views as ProductsViews
+
+router = DefaultRouter()
+router.register(r'users', AccountsViews.UserViewSet)
+router.register(r'mailingaddress', AccountsViews.MailingAddressViewSet)
+router.register(r'products', ProductsViews.ProductViewSet)
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('accounts.urls')),
-    url(r'^', include('products.urls')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))   
 ]
