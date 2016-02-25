51deb0d2e38fc1b9dcbb6004494e45cac1c98d24
cassac <hicassac@gmail.com>
2016-02-25 14:30:40 -0800

    adds serializer_context to allow user/{pk}/mailingaddress resource extension

--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,28 @@
 from django.contrib.auth.models import User
-from accounts.permissions import IsOwnerOrReadOnly, EditIfUserIsAdminOrUser
+from accounts.permissions import ViewEditIfAdminOrUser
 from accounts.models import MailingAddress
 from accounts.serializers import MailingAddressSerializer, UserSerializer
 from rest_framework import generics, viewsets, permissions
 from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
+from rest_framework.request import Request
 
 class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-	                     EditIfUserIsAdminOrUser)
+	permission_classes = (permissions.IsAdminUser,)
 
-	@detail_route(methods=['GET', 'PUT'], permission_classes=[], url_path='mailingaddress')
+	@detail_route(methods=['GET', 'PUT'], permission_classes=[ViewEditIfAdminOrUser])
 	def mailingaddress(self, request, pk):
+		serializer_context = {
+		    'request': Request(request),
+		}
 		queryset = User.objects.get(pk=pk).mailingaddress_set.all()
-		serializer = MailingAddressSerializer(queryset, many=True, read_only=True)
+		serializer = MailingAddressSerializer(queryset, many=True, read_only=True, context=serializer_context)
 		return Response(serializer.data)
 
 class MailingAddressViewSet(viewsets.ModelViewSet):
 
 	queryset = MailingAddress.objects.all()
 	serializer_class = MailingAddressSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-	                      IsOwnerOrReadOnly,)
-	def perform_create(self, instance):
-		serializer = MailingAddressSerializer(instance, partial=True)
-		serializer.save(owner=self.request.user)
+	permission_classes = (permissions.IsAdminUser,)
