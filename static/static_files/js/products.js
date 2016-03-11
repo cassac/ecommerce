55f21f4d9393bd0267a69b22d7c63412e00bce8f
cassac <hicassac@gmail.com>
2016-03-11 08:39:10 -0800

    starts ajax GET

--- static/static_files/js/products.js
+++ static/static_files/js/products.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	alert('hello world');
+
+	$.ajax({
+		url:'/productvariation/',
+		type: 'GET',
+		contentType: 'application/json;charset=UTF-8',
+  		dataType: "json",
+  		success: function(data){
+  			$.each(data, function(index, value) {
+  				console.log(index, value);
+  			});
+  		},
+  		error: function(error){
+  			console.log(error);
+  		} 
+	}) // end ajax
+
 });
