c94d918c0b9cba18a2ba890ebb12c7d66d9c9661
cassac <hicassac@gmail.com>
2016-03-10 17:43:04 -0800

    writes first test for user creation

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.conf import settings
+# from .models import 
 
-# Create your tests here.
+class UserTestCase(TestCase):
+	def setUp(self):
+		u = User.objects.create(
+			first_name='Test',
+			last_name='User',
+			username='testuser',
+			email='test@example.com',
+			)
+		u.save()
+		u.set_password('testuser')
+
+	def test_user_created(self):
+		user = User.objects.first()
+		self.assertEqual(user.username, 'testuser')
