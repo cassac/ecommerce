413626055bfa1f91f3b9420666cd2234e30d9373
cassac <hicassac@gmail.com>
2016-03-12 09:23:05 -0800

    adds base serializers

---
+++ ecommerce/carts/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from carts.models import Cart, CartItem
+
+
+class CartSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Cart
+		fields = ('__all__')
+
+class CartItemSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = CartItem
+		fields = ('__all__')
