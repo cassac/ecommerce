cea9e1627deabb7aff74e5ba4c2e31d6588edd20
cassac <hicassac@gmail.com>
2016-03-11 16:45:26 -0800

    loops through ajax returned products and appends templates

--- static/static_files/js/products.js
+++ static/static_files/js/products.js
@@ -1,14 +1,42 @@
+
+
+var insertUpperCarousel = function(products) {
+
+	counter = 1;
+
+	$.each(products, function(index, value) {
+
+  		item = columnItem(
+  			value.productimage_set[0].image, 
+  			value.title, 
+  			value.price
+  			);
+
+  		if (!counter%3) {
+  			carouselfRowHead += item;
+  		} else {
+  			carouselfRowHead += item;
+  			$('#upperCarousel').append(carouselfRowHead + carouselfRowTail);
+  			// resets template head and tail which are imported from template.js
+  			carouselfRowHead = '<div class="item"><div class="row">';
+			carouselfRowTail = '</div></div>';
+  		}
+		
+		counter++;
+
+	});
+
+}
+
 $(document).ready(function(){
 
 	$.ajax({
-		url:'/productvariation/',
+		url:'/products/',
 		type: 'GET',
 		contentType: 'application/json;charset=UTF-8',
   		dataType: "json",
   		success: function(data){
-  			$.each(data, function(index, value) {
-  				console.log(index, value);
-  			});
+  			insertUpperCarousel(data);
   		},
   		error: function(error){
   			console.log(error);
