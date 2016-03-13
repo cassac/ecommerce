d122ffeede731d936f1b4347182f73a8c640ac23
cassac <hicassac@gmail.com>
2016-03-12 17:22:40 -0800

    adds url to use for add/remove items from cart

---
+++ ecommerce/carts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+	url(r'^item/(?P<product_variation_id>[0-9]*)/$', views.CartItemView.as_view(), name='cart-item'),
+]
